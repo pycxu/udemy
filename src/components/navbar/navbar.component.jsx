98a1641b5413fe2471958c156ef8374430c5ed6d
Paul <Paul@xycdeMacBook-Pro.local>
2019-09-12 21:31:25 +1000

    fixed dropdown

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -12,13 +12,13 @@ class NavBar extends React.Component {
 
     render () {
         return (
-            <div className="nav" aria-label="Main navigation" role="navigation">
+            <div className="c_header c_header--v6 c_header--desktop c_header--ia c_header--ia- ud-app-loader ud-app-loaded" aria-label="Main navigation" role="navigation">
 
-                <div className="nav__inner" role="navigation">
+                <div className="c_header__inner" role="navigation">
 
-                    <div className="nav__logo-container">
+                    <div className="c_header__logo-container">
                         <a 
-                            className="nav__logo-wrap"
+                            className="c_header__logo-wrap"
                             href="/"
                             target="_self"
                             rel=" noopener noreferrer"
@@ -33,10 +33,10 @@ class NavBar extends React.Component {
                         </a>
                     </div>
 
-                    <div className="nav__left">
+                    <div className="c_header__left">
                         <div className="ud-component--header-v6--browse-dropdown-button hidden-xs hidden-xxs">
                             <div className="dropdown--open-on-hover dropdown--topics dropdown--open-on-hover dropdown">
-                                <a   href="javascript:void(0)" id="nav.browse" role="button" className="dropdown-toggle" aria-haspopup="true" aria-expanded="false">
+                                <a href="javascript:void(0)" id="header.browse" role="button" className="dropdown-toggle" aria-haspopup="true" aria-expanded="false">
                                     <span className="udi udi-explore"></span>
                                     <span> Categories</span>
                                 </a>
@@ -45,23 +45,23 @@ class NavBar extends React.Component {
                                         <ul className="dropdown-menu__list dropdown-menu__list--level-one" role="dialog" aria-label="Categories">
                                             <li className="menu__link" data-purpose="ver-development" role="presentation">
                                                 <a href="/courses/development/" aria-expanded="false" role="button" target="_self">
-                                                    <span data-purpose="menu-icon" class="menu__icon udi udi-development"></span>
-                                                    <span class="fx">Development</span>
-                                                    <span data-purpose="menu-item-arrow" class="menu__arrow udi udi-next"></span>
+                                                    <span data-purpose="menu-icon" className="menu__icon udi udi-development"></span>
+                                                    <span className="fx">Development</span>
+                                                    <span data-purpose="menu-item-arrow" className="menu__arrow udi udi-next"></span>
                                                 </a>
                                             </li>
                                             <li className="menu__link" data-purpose="ver-business" role="presentation">
                                                 <a href="/courses/business/" aria-expanded="false" role="button" target="_self">
-                                                    <span data-purpose="menu-icon" class="menu__icon udi udi-Business"></span>
-                                                    <span class="fx">Business</span>
-                                                    <span data-purpose="menu-item-arrow" class="menu__arrow udi udi-next"></span>
+                                                    <span data-purpose="menu-icon" className="menu__icon udi udi-Business"></span>
+                                                    <span className="fx">Business</span>
+                                                    <span data-purpose="menu-item-arrow" className="menu__arrow udi udi-next"></span>
                                                 </a>
                                             </li>
                                             <li className="menu__link" data-purpose="ver-finance-and-accounting" role="presentation">
                                                 <a href="/courses/finance-and-accounting/" aria-expanded="false" role="button" target="_self">
-                                                    <span data-purpose="menu-icon" class="menu__icon udi udi-finance"></span>
-                                                    <span class="fx">Finance & Accounting</span>
-                                                    <span data-purpose="menu-item-arrow" class="menu__arrow udi udi-next"></span>
+                                                    <span data-purpose="menu-icon" className="menu__icon udi udi-finance"></span>
+                                                    <span className="fx">Finance & Accounting</span>
+                                                    <span data-purpose="menu-item-arrow" className="menu__arrow udi udi-next"></span>
                                                 </a>
                                             </li>
                                         </ul>
